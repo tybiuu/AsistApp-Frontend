f6b5434e9e8812fc931ed501ec8852fa258d7d63
esteban <esteban20001118@gmail.com>
2026-05-17 00:35:00 -0500

    add: refactor PrimaryButton component and implement RoleSelect functionality with navigation
    Co-authored-by: tybiuu <tcasafrancacarrillo@gmail.com>

--- lib/configs/routes.dart
+++ lib/configs/routes.dart
@@ -6,5 +6,6 @@ class AppRoutes {
   static const String onboarding = '/onboarding';
   static const String login = '/login';
   static const String roleSelect = '/role-select';
+  static const String register = '/register';
   static const String home = '/home';
 }
